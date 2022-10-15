f1cdc292eca3e45d1770a12a0131e126471fbd63
KtwoNn <khangnguyent.2002@gmail.com>
2022-10-15 18:56:50 -0500

    Did some stuff after trolling

---
+++ backend/MultiData.py
@@ -0,0 +1,57 @@
+
+
+from werkzeug.utils import secure_filename
+from flask import Flask, render_template, request
+import cv2
+import os
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return "Please upload file"
+
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    if request.files.get("video") is None:
+        return render_template("my_error_page.html", an_optional_message="Error uploading file")
+    # Put here some other checks (security, file length etc...)
+    f = request.files["video"]
+    f.save(secure_filename(f.filename))
+
+    cam = cv2.VideoCapture(f.filename)
+    
+    currentframe = 0
+    listOfFrames = []
+    while(True):
+    # reading from frame
+        ret,frame = cam.read()
+  
+        if ret:
+        # if video is still left continue creating images
+            name = './data/frame' + str(currentframe) + '.jpg'
+            print ('Creating...' + name)
+  
+        # writing the extracted images
+            cv2.imwrite(name, frame)
+
+            listOfFrames.append(name)
+  
+        # increasing counter so that it will
+        # show how many frames are created
+            currentframe += 1
+        else:
+            break
+  
+
+    f.stream.seek(0)
+    content = ""
+    for line in f.stream.readlines():
+        content += line.decode("UTF-8") # Decode here as needed
+    print(content)
+
+if __name__ == '__main__':
+   app.run()
